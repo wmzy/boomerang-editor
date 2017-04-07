4fceeedb647d1f4994dc6511ef0e882991b209a1
zhuletian <zhuletian@xiaomi.com>
2017-04-07 15:59:52 +0800

    public path from env

--- config/index.js
+++ config/index.js
@@ -1,5 +1,6 @@
 // see http://vuejs-templates.github.io/webpack for documentation.
 var path = require('path')
+var PUBLIC_PATH = process.env.PUBLIC_PATH;
 
 module.exports = {
   build: {
@@ -7,7 +8,7 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: PUBLIC_PATH || '/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
@@ -26,7 +27,7 @@ module.exports = {
     port: 8080,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: PUBLIC_PATH || '/',
     proxyTable: {},
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
